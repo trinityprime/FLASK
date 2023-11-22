ca5ac93cf8dd1f139b3a766b203267105cd25241
trinityprime <limkl.ryan@gmail.com>
2023-11-22 11:09:11 +0800

    FIRST COMMIT

--- app.py
+++ app.py
@@ -5,4 +5,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, flask!"
+    return "Hello"
+
+
+@app.route('/index')
+def main():
+    return "Main"
+
+
+@app.route('/user/<username>')
+def user(username):
+    return f'{username.upper()}\'s profile'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
